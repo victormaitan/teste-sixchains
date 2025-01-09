fdd80d6f90d074173498a4d89c6796da2f37e1c3
maitan <victormaitan01@gmail.com>
2025-01-09 17:01:47 -0400

    Adicionado bottom sheet para inserir nova tarefa

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,13 +1,13 @@
 <template>
-    <v-container>
-        <v-card :ripple="false" class="mt-3 pa-2" rounded="xl" v-for="task in tasks" :key="task.id"
+    <v-container class="mb-14" style="overflow-y: auto; height: 78vh;">
+        <v-card :ripple="false" color="primary" class="mt-3 pa-2" rounded="xl" v-for="task in tasks" :key="task.id"
             @click="editTask(task)">
             <v-row class="d-flex justify-space-between align-center">
                 <v-col cols="10">
                     <v-checkbox hide-details :label="task.description" />
                 </v-col>
-                <v-col cols="2">
-                    <v-btn class="mr-2" icon variant="tonal" density="comfortable" @click.stop="deleteTask(task.id)">
+                <v-col cols="2" class="d-flex justify-end">
+                    <v-btn class="mr-2" icon variant="tonal" color="red" density="comfortable" @click.stop="deleteTask(task.id)">
                         <v-icon>mdi-delete</v-icon>
                     </v-btn>
                 </v-col>
@@ -23,18 +23,48 @@ import { onMounted, ref } from 'vue'
 
 const tasks = ref([
     {
-        id: 'fewf23f21w143y9fj',
+        id: 'fewf23f21sw143y9fj',
         description: 'Tarefa 1',
         done: false
     },
     {
-        id: 'sdaf13f3254u6435y2q4',
+        id: 'sdaf13f3g254u6435y2q4',
+        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
+        done: false
+    },
+    {
+        id: 'fewf23f21wh143y9fj',
+        description: 'Tarefa 2',
+        done: false
+    },
+    {
+        id: 'sdaf13f3g254u643a5y2q4',
+        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
+        done: false
+    },
+    {
+        id: 'fewf23f21wh1a43y9fj',
+        description: 'Tarefa 2',
+        done: false
+    },
+    {
+        id: 'sdaf13sf3254u6435y2q4',
+        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
+        done: false
+    },
+    {
+        id: 'fewf23f21hw143y9fj',
+        description: 'Tarefa 3',
+        done: false
+    },
+    {
+        id: 'sdaf13f3d254u6435y2q4',
         description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
         done: false
     },
     {
         id: '254n247547m45',
-        description: 'Tarefa 3',
+        description: 'Tarefa 4',
         done: false
     }
 ])
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,51 @@
 <template>
-    <v-container class="full-height">
-        <v-row cols="auto" class="d-flex justify-space-between align-center mx-2">
-            <v-col>
-                <h1>Hoje</h1>
-                <h4 class="font-weight-regular text-grey-lighten-1">{{ new Date().toLocaleDateString('pt-BR', {
-                    weekday:
-                        'long', day: '2-digit', month: 'long', year: 'numeric'
-                }) }}</h4>
-            </v-col>
-            <v-btn class="ma-2" icon="mdi-cog" size="small"></v-btn>
-        </v-row>
-        <v-tabs v-model="tab" color="primary" align-tabs="center">
-            <v-tab prepend-icon="mdi-book-open-page-variant" text="A fazer" value="a-fazer"></v-tab>
-            <v-tab prepend-icon="mdi-handshake" text="Concluídos" value="concluidos"></v-tab>
-        </v-tabs>
+    <div height="98vh" class="pa-2 d-flex justify-space-between">
+        <div>
+            <v-row cols="auto" class="d-flex justify-space-between align-center mx-2">
+                <v-col>
+                    <h1>Hoje</h1>
+                    <h4 class="font-weight-regular text-grey-lighten-1">{{ new Date().toLocaleDateString('pt-BR', {
+                        weekday:
+                            'long', day: '2-digit', month: 'long', year: 'numeric'
+                    }) }}</h4>
+                </v-col>
+                <v-btn class="ma-2" icon="mdi-cog" size="small" variant="tonal" tile rounded="lg"></v-btn>
+            </v-row>
+            <v-tabs v-model="tab" color="primary" align-tabs="center">
+                <v-tab prepend-icon="mdi-book-open-page-variant" text="A fazer" value="a-fazer"></v-tab>
+                <v-tab prepend-icon="mdi-handshake" text="Concluídos" value="concluidos"></v-tab>
+            </v-tabs>
 
-        <v-tabs-window v-model="tab">
-            <v-tabs-window-item value="a-fazer">
-                <TaskList @taskEdit="editTask" />
-            </v-tabs-window-item>
-            <v-tabs-window-item value="concluidos">
-                <TaskList @taskEdit="editTask" />
-            </v-tabs-window-item>
-        </v-tabs-window>
+            <v-tabs-window v-model="tab">
+                <v-tabs-window-item value="a-fazer">
+                    <TaskList @taskEdit="editTask" />
+                </v-tabs-window-item>
+                <v-tabs-window-item value="concluidos">
+                    <TaskList @taskEdit="editTask" />
+                </v-tabs-window-item>
+            </v-tabs-window>
+        </div>
         <!-- <TaskList @taskEdit="editTask" /> -->
         <!-- <TaskForm @taskSaved="fetchTasks" /> -->
-        </v-container>
-        <v-fab color="primary" icon="mdi-playlist-edit" class="ms-4 mb-4" size="64" absolute layout></v-fab>
+        <v-bottom-sheet>
+            <template v-slot:activator="{ props }">
+                <v-btn v-bind="props" variant="elevated" rounded="lg" position="fixed" location="bottom" class="mb-4"
+                    color="primary" size="large" prepend-icon="mdi-playlist-edit">Nova Tarefa</v-btn>
+            </template>
+
+            <v-card title="Nova Tarefa" rounded="t-xl" class="glass">
+                <v-textarea class="mx-6" clearable variant="plain" placeholder="Escreva aqui..." auto-grow></v-textarea>
+                <v-row class="ma-4 d-flex justify-end ga-2">
+                    <v-btn flat color="grey-lighten-2">
+                        Cancelar
+                    </v-btn>
+                    <v-btn color="success">
+                        Salvar
+                    </v-btn>
+                </v-row>
+            </v-card>
+        </v-bottom-sheet>
+    </div>
 </template>
 
 <script setup>
@@ -46,5 +65,13 @@ const editTask = (task) => {
 .tabButton {
     color: white;
     background-color: rgb(var(--v-theme-primary));
+}
+
+.glass {
+    /* From https://css.glass */
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
 }
 </style>
