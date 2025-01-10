613ab97c8f29272f07a646fab55260ac2e565f97
Maitan <victormaitan01@gmail.com>
2025-01-10 02:57:03 -0400

    Inserido credenciais para host de teste

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,12 +7,14 @@ export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
   
   const firebaseConfig = {
-    apiKey: config.public.firebaseApiKey,
-    authDomain: config.public.firebaseAuthDomain,
-    projectId: config.public.firebaseProjectId,
-    storageBucket: config.public.firebaseStorageBucket,
-    messagingSenderId: config.public.firebaseMessagingSenderId,
-    appId: config.public.firebaseAppId,
+    apiKey: "AIzaSyB_WwAb96xo_0ve2NkwvymyQU3K4ty-LF8",
+    authDomain: "teste-sistema-de-vendas.firebaseapp.com",
+    databaseURL: "https://teste-sistema-de-vendas-default-rtdb.firebaseio.com",
+    projectId: "teste-sistema-de-vendas",
+    storageBucket: "teste-sistema-de-vendas.appspot.com",
+    messagingSenderId: "842276438092",
+    appId: "1:842276438092:web:404e959285a3f4cc0474e9",
+    measurementId: "G-V6Q3GSF6RK"
   };
 
   // Inicialize o Firebase
