dcba7132fee6f69d313b43dbf9711211b9f9b89c
Maitan <victormaitan01@gmail.com>
2025-01-10 02:40:01 -0400

    Corrigido problema com variaveis de ambiente

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,20 +1,32 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 export default defineNuxtPlugin(() => {
-  const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  }
+  
+  const config = useRuntimeConfig();
+  
+  const firebaseConfig = {
+    apiKey: config.public.firebaseApiKey,
+    authDomain: config.public.firebaseAuthDomain,
+    projectId: config.public.firebaseProjectId,
+    storageBucket: config.public.firebaseStorageBucket,
+    messagingSenderId: config.public.firebaseMessagingSenderId,
+    appId: config.public.firebaseAppId,
+  };
 
-  const app = initializeApp(config)
-  const db = getFirestore(app)
+  // Inicialize o Firebase
+  const app = initializeApp(firebaseConfig);
 
+  // Inicialize Firestore e Auth
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  // Adiciona as instâncias de db e auth ao Nuxt
   return {
-    provide: { db },
-  }
-})
+    provide: {
+      db,
+      auth,
+    },
+  };
+});
