206c8c60e356c9407ddbf0337dffbaf1047b22be
Maitan <victormaitan01@gmail.com>
2025-01-10 02:43:46 -0400

    Configuracao do tema do app

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,21 +1,22 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-import colors from 'vuetify/util/colors'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import colors from "vuetify/util/colors";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const vuetify = createVuetify({
-        theme: {
-            themes: {
-                light: {
-                    dark: false,
-                    colors: {
-                        primary: colors.purple.lighten4,
-                        secondary: colors.red.lighten4,
-                    }
-                },
-            },
+  const vuetify = createVuetify({
+    theme: {
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.purple.lighten3,
+            secondary: colors.green.lighten4,
+            accent: colors.purple.lighten4,
+          },
         },
-    })
-    nuxtApp.vueApp.use(vuetify)
-})
+      },
+    },
+  });
+  nuxtApp.vueApp.use(vuetify);
+});
