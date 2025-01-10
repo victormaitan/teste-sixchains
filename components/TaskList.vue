85a9df129d35056fb35e635097b5ce7c1f781d47
Maitan <victormaitan01@gmail.com>
2025-01-10 02:42:30 -0400

    Refatorada a lista de tarefas

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,91 +1,99 @@
 <template>
-    <v-container class="mb-14" style="overflow-y: auto; height: 78vh;">
-        <v-card :ripple="false" color="primary" class="mt-3 pa-2" rounded="xl" v-for="task in tasks" :key="task.id"
-            @click="editTask(task)">
-            <v-row class="d-flex justify-space-between align-center">
-                <v-col cols="10">
-                    <v-checkbox hide-details :label="task.description" />
-                </v-col>
-                <v-col cols="2" class="d-flex justify-end">
-                    <v-btn class="mr-2" icon variant="tonal" color="red" density="comfortable" @click.stop="deleteTask(task.id)">
-                        <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                </v-col>
-            </v-row>
-        </v-card>
-    </v-container>
+  <v-container class="mb-14" style="overflow-y: auto; height: 78vh">
+    <v-card
+      :ripple="false"
+      :color="task.done ? 'grey-lighten-2' : 'accent'"
+      class="mt-3 pa-2"
+      rounded="xl"
+      v-for="task in tasks"
+      :key="task.id"
+    >
+      <v-row class="d-flex justify-space-between align-center">
+        <v-col cols="2">
+          <v-checkbox v-model="task.done" :color="task.done ? 'grey-lighten-1' : ''" hide-details @click="editTask(task, 'check')" />
+        </v-col>
+        <v-col
+          cols="8"
+          @click="editTask(task, 'update')"
+          style="cursor: pointer"
+        >
+          <p :class="`text-subtitle-1 ${task.done ? 'text-grey-lighten-1 text-decoration-line-through' : ''}`">{{ task.description }}</p>
+        </v-col>
+        <v-col cols="2" class="d-flex justify-end">
+          <v-dialog v-model="editDialog" max-width="600">
+            <v-card
+              prepend-icon="mdi-playlist-edit"
+              title="Editar Tarefa"
+              class="glass"
+              rounded="xl"
+            >
+              <v-card-text>
+                <v-row dense>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      placeholder="Descrição"
+                      required
+                      v-model="editedTask.description"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  text="Cancelar"
+                  variant="plain"
+                  @click="editDialog = false"
+                ></v-btn>
+
+                <v-btn
+                  color="success"
+                  text="Salvar"
+                  variant="tonal"
+                  @click="
+                    updateTask(editedTask.id, editedTask), (editDialog = false)
+                  "
+                ></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-btn
+            class="mr-2"
+            icon
+            variant="tonal"
+            :color="task.done ? 'grey-lighten-1' : 'red'"
+            density="comfortable"
+            @click.stop="deleteTask(task.id)"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-// import { useNuxtApp } from '#app'
-// import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'
+import { onMounted } from "vue";
+import { useTasks } from "~/composables/useTasks";
 
-const tasks = ref([
-    {
-        id: 'fewf23f21sw143y9fj',
-        description: 'Tarefa 1',
-        done: false
-    },
-    {
-        id: 'sdaf13f3g254u6435y2q4',
-        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
-        done: false
-    },
-    {
-        id: 'fewf23f21wh143y9fj',
-        description: 'Tarefa 2',
-        done: false
-    },
-    {
-        id: 'sdaf13f3g254u643a5y2q4',
-        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
-        done: false
-    },
-    {
-        id: 'fewf23f21wh1a43y9fj',
-        description: 'Tarefa 2',
-        done: false
-    },
-    {
-        id: 'sdaf13sf3254u6435y2q4',
-        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
-        done: false
-    },
-    {
-        id: 'fewf23f21hw143y9fj',
-        description: 'Tarefa 3',
-        done: false
-    },
-    {
-        id: 'sdaf13f3d254u6435y2q4',
-        description: 'Não esquecer de levar a canela no veterinario hoje as 14h,Não esquecer de levar a canela no veterinario hoje as 14h',
-        done: false
-    },
-    {
-        id: '254n247547m45',
-        description: 'Tarefa 4',
-        done: false
-    }
-])
-// const { $db } = useNuxtApp()
+const { tasks, fetchTasks, updateTask, deleteTask } = useTasks();
+const editDialog = ref(false);
+const editedTask = ref({});
 
-const fetchTasks = async () => {
-    // const querySnapshot = await getDocs(collection($db, 'tasks'))
-    // tasks.value = querySnapshot.docs.map(doc => ({
-    //   id: doc.id,
-    //   ...doc.data(),
-    // }))
+onMounted(() => {
+  fetchTasks();
+});
+
+function editTask(task, type) {
+  editedTask.value = { ...task };
+  if (type === "update") {
+    editDialog.value = true;
+  } else if (type === "check") {
+    task.done = !task.done;
+    updateTask(task.id, task);
+  }
 }
-
-const deleteTask = async (id) => {
-    // await deleteDoc(doc($db, 'tasks', id))
-    // fetchTasks()
-}
-
-const editTask = (task) => {
-    // Emit task data for editing
-}
-
-onMounted(fetchTasks)
 </script>
